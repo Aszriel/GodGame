620d8294ddf9fb1c858d3cc62c161f76b255d08f
Gregoire <guille.gregoire@orange.fr>
2019-12-02 18:39:46 +0100

    Début d'une modification pour la reproduction

--- GodGame/GestVie.cs
+++ GodGame/GestVie.cs
@@ -6,13 +6,13 @@ namespace GodGame
 {
     class GestVie<T> where T : EtreVivant
     {
-        private List<T> m_listEtreVivant;
+        private List<EtreVivant> m_listEtreVivant;
 
         /// <summary>
         /// Constructeur prenant en entrée une liste d'être vivant à animer
         /// </summary>
         /// <param name="p_listEtreVivant">La liste d'êtres vivants à animer</param>
-        public GestVie(List<T> p_listEtreVivant)
+        public GestVie(List<EtreVivant> p_listEtreVivant)
         {
             m_listEtreVivant = p_listEtreVivant;
         }
@@ -25,7 +25,7 @@ namespace GodGame
          Console.WriteLine("Choix numéro 3 : Quitter le programme");
         }
 
-        public static void ShowEtrevivant(List<T> p_listEtreVivant)
+        public static void ShowEtrevivant(List<EtreVivant> p_listEtreVivant)
         {
             int nombreMort = 0;
             int nombreEnVie = 0;
@@ -50,6 +50,7 @@ namespace GodGame
             int Choix = 0;
             int ChoixAleatoire;
             int nombreEtreVivant = 1;
+            int nombreTuer = 0;
             Random aleatoire = new Random();
             Menu();
             do
@@ -63,10 +64,19 @@ namespace GodGame
                         {
                             //Génération d'un nombre aléatoire entre 1 et le nombre d'etre vivant 
                             nombreEtreVivant = m_listEtreVivant.Count;
-                            ChoixAleatoire = aleatoire.Next(1, nombreEtreVivant);
-                            EtreVivant.Tuer(m_listEtreVivant[ChoixAleatoire]);
-                            Console.WriteLine($"{m_listEtreVivant[ChoixAleatoire].Nom} a ete tue. ");            
-                            ShowEtrevivant(m_listEtreVivant);
+                            nombreTuer++; 
+                            //On génére un nouveau nombre aléatoire si etat = false et seulement si tous les etre ne sont pas déjà mort
+                            if (nombreTuer!=nombreEtreVivant)
+                            {
+                                do
+                                {
+                                    ChoixAleatoire = aleatoire.Next(1, nombreEtreVivant);
+                                }while(m_listEtreVivant[ChoixAleatoire].Etat==false);
+                                EtreVivant.Tuer(m_listEtreVivant[ChoixAleatoire]);
+                                Console.WriteLine($"{m_listEtreVivant[ChoixAleatoire].Nom} a ete tue. ");            
+                                ShowEtrevivant(m_listEtreVivant);  
+                            }else
+                            Console.WriteLine("Impossible de tuer un etre vivant, il ne reste plus personne!");
                         }
                         break;
 
@@ -76,6 +86,15 @@ namespace GodGame
                             //Génération d'un nombre aléatoire
                             nombreEtreVivant = m_listEtreVivant.Count;
                             ChoixAleatoire = aleatoire.Next(1, nombreEtreVivant);
+                            int ChoixAleatoire2 = aleatoire.Next(1, nombreEtreVivant);
+                            if(m_listEtreVivant[ChoixAleatoire].GetType() == m_listEtreVivant[ChoixAleatoire2].GetType())
+                            {
+                                
+                            }
+                            else
+                            {
+                                Console.WriteLine("La reproduction entre ",m_listEtreVivant[ChoixAleatoire].Nom ,"et", m_listEtreVivant[ChoixAleatoire2].Nom, "a échoué");
+                            }
                         }
                         break;
 
